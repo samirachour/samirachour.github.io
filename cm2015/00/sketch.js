6f88759533a25de387a05e261a0406e21e21fcfc
samirachour <achoursamir82@gmail.com>
2016-09-28 10:54:13 -0500

    index

---
+++ cm2015/00/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  //background(200,255,0);
+}
+function touchStarted() {
+  background(random(0,255),random(0,255),random(0,255));
+}
