ab05777d2e4e41b71bf5b84e88e6037561209d32
samirachour <achoursamir82@gmail.com>
2016-10-05 10:29:20 -0500

    line

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -1,6 +1,7 @@
 document.ontouchmove = function(event){
   event.preventDefault();
 }
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
   rect(CENTER);
@@ -14,9 +15,11 @@ function draw() {
   
   for(var i=0;i<touches.length;i++){
     rect(touches[i].x,touches[i].y,150,150);
+    
   }
 }
 
 function touchStarted() {
+  line(25,25,75,75);
   
 }
