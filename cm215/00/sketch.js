a2a359df62a29b251c632c864c452a2c4523abeb
samirachour <achoursamir82@gmail.com>
2016-10-18 18:47:58 -0500

    ghjhtjkty

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -15,24 +15,20 @@ function draw() {
   
   for(var i=0;i<touches.length;i++){
     if((i+1)<touches.length){
-      beginShape(TRIANGLES);
-vertex(30, 75);
-vertex(40, 20);
-vertex(50, 75);
-vertex(60, 20);
-vertex(70, 75);
-vertex(80, 20);
+      beginShape(LINES);
+vertex(touches[i].30, touches[i].20);
+vertex(touches[i].85, touches[i].20);
+vertex(touches[i+1].85, touches[i+1].75);
+vertex(touches[i+1].30, touches[i+1].75);
 endShape();
     //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
     }
  else{
-beginShape(TRIANGLES);
-vertex(30, 75);
-vertex(40, 20);
-vertex(50, 75);
-vertex(60, 20);
-vertex(70, 75);
-vertex(80, 20);
+beginShape(LINES);
+vertex(touches[i].30, touches[i].20);
+vertex(touches[i].85, touches[i].20);
+vertex(touches[i+1].85, touches[i+1].75);
+vertex(touches[i+1].30, touches[i+1].75);
 endShape();
    // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
 }
