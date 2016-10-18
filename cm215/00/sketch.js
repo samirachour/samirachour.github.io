51c010e24afbf109b9255cec16042fc2e1632330
samirachour <achoursamir82@gmail.com>
2016-10-18 18:24:15 -0500

    kgkgk

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -17,16 +17,19 @@ function draw() {
     if((i+1)<touches.length){
       beginShape(TRIANGLES);
 vertex(touches[i+1].x,touches[i+1].y);
-vertex(touches[i+2].x,touches[i+2].y);
-vertex(touches[i+3].x,touches[i+3].y);
-//vertex(60, 20);
-//vertex(70, 75);
-//vertex(80, 20);
+vertex(touches[i+1].x,touches[i+1].y);
+vertex(touches[i+1].x,touches[i+1].y);
+vertex(touches[i+1].x,touches[i+1].y);
+vertex(touches[i+1].x,touches[i+1].y);
+vertex(touches[i+1].x,touches[i+1].y);
 endShape();
     //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
     }
  else{
 beginShape(TRIANGLES);
+vertex(touches[0].x,touches[0].y);
+vertex(touches[0].x,touches[0].y);
+vertex(touches[0].x,touches[0].y);
 vertex(touches[0].x,touches[0].y);
 vertex(touches[0].x,touches[0].y);
 vertex(touches[0].x,touches[0].y);
