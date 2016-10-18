f1435146766b00265ba22e8d5d2f3b0ded2a6e47
samirachour <achoursamir82@gmail.com>
2016-10-18 18:53:26 -0500

    gggfr

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -16,19 +16,19 @@ function draw() {
   for(var i=0;i<touches.length;i++){
     if((i+1)<touches.length){
       beginShape(LINES);
-vertex(touches[i].30, touches[i].20);
-vertex(touches[i].85, touches[i].20);
-vertex(touches[i+1].85, touches[i+1].75);
-vertex(touches[i+1].30, touches[i+1].75);
+vertex(30, 20, width/2, height/2);
+vertex(85, 20, width/2, height/2);
+vertex(85, 75, width/2, height/2);
+vertex(30, 75, width/2, height/2);
 endShape();
     //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
     }
  else{
 beginShape(LINES);
-vertex(touches[i].30, touches[i].20);
-vertex(touches[i].85, touches[i].20);
-vertex(touches[i+1].85, touches[i+1].75);
-vertex(touches[i+1].30, touches[i+1].75);
+vertex(30, 20, width/2, height/2);
+vertex(85, 20, width/2, height/2);
+vertex(85, 75, width/2, height/2);
+vertex(30, 75, width/2, height/2);
 endShape();
    // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
 }
