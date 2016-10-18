87c22de492364f0c3f6614ad2231f2965b609a7b
samirachour <achoursamir82@gmail.com>
2016-10-18 17:42:18 -0500

    infusa

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -20,6 +20,7 @@ function draw() {
   else{ 
     line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
   }
+  fill(random(0,255),random(0,255),random(0,255));
     rect(touches[i].x,touches[i].y,150,150);
    
   }
