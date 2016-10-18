c63274d2cf16c0fd1018e26cc571b3b982abbde1
samirachour <achoursamir82@gmail.com>
2016-10-18 17:50:41 -0500

    ahhhh

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -4,7 +4,7 @@ document.ontouchmove = function(event){
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  rectMode(CENTER);
+  //rectMode(CENTER);
   stroke(0);
   noFill();
   strokeWeight(5);
@@ -20,8 +20,8 @@ function draw() {
   else{ 
     line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
   }
-  fill(random(0,255),random(0,255),random(0,255));
-    rect(touches[i].x,touches[i].y,150,150);
+                                   
+    ellipse(touches[i].x,touches[i].y,150,150);
    
   }
 }
