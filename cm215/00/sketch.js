c2998e1dbe4382edd6af05b70fe7cd0d1924917a
samirachour <achoursamir82@gmail.com>
2016-10-18 18:25:55 -0500

    hgfd

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -16,23 +16,23 @@ function draw() {
   for(var i=0;i<touches.length;i++){
     if((i+1)<touches.length){
       beginShape(TRIANGLES);
-vertex(touches[i+1].x,touches[i+1].y);
-vertex(touches[i+1].x,touches[i+1].y);
-vertex(touches[i+1].x,touches[i+1].y);
-vertex(touches[i+1].x,touches[i+1].y);
-vertex(touches[i+1].x,touches[i+1].y);
-vertex(touches[i+1].x,touches[i+1].y);
+vertex(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
+vertex(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
+vertex(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
+vertex(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
+vertex(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
+vertex(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
 endShape();
     //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
     }
  else{
 beginShape(TRIANGLES);
-vertex(touches[0].x,touches[0].y);
-vertex(touches[0].x,touches[0].y);
-vertex(touches[0].x,touches[0].y);
-vertex(touches[0].x,touches[0].y);
-vertex(touches[0].x,touches[0].y);
-vertex(touches[0].x,touches[0].y);
+vertex(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
+vertex(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
+vertex(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
+vertex(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
+vertex(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
+vertex(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
 endShape();
    // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
 }
