7ef56abff7fdb4bd87106d799fd8669c2557bb2a
samirachour <achoursamir82@gmail.com>
2016-10-18 18:37:34 -0500

    fhhgdfnnb

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -16,23 +16,23 @@ function draw() {
   for(var i=0;i<touches.length;i++){
     if((i+1)<touches.length){
       beginShape(TRIANGLES);
-vertex(touches[i].x,touches[i].y,30, 75);
-vertex(touches[i].x,touches[i].y,40, 20);
-vertex(touches[i].x,touches[i].y,50, 75);
-vertex(touches[i].x,touches[i].y,60, 20);
-vertex(touches[i].x,touches[i].y,70, 75);
-vertex(touches[i].x,touches[i].y,80, 20);
+vertex(30, 75);
+vertex(40, 20);
+vertex(50, 75);
+vertex(60, 20);
+vertex(70, 75);
+vertex(80, 20);
 endShape();
     //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
     }
  else{
 beginShape(TRIANGLES);
-vertex(touches[i].x,touches[i].y,30, 75);
-vertex(touches[i].x,touches[i].y,40, 20);
-vertex(touches[i].x,touches[i].y,50, 75);
-vertex(touches[i].x,touches[i].y,60, 20);
-vertex(touches[i].x,touches[i].y,70, 75);
-vertex(touches[i].x,touches[i].y,80, 20);
+vertex(30, 75);
+vertex(40, 20);
+vertex(50, 75);
+vertex(60, 20);
+vertex(70, 75);
+vertex(80, 20);
 endShape();
    // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
 }
