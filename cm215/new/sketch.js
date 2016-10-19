37fe33d6f326f016773b7f39ca8bd637571366d3
samirachour <achoursamir82@gmail.com>
2016-10-19 10:20:28 -0500

    ghjgfh

--- cm215/new/sketch.js
+++ cm215/new/sketch.js
@@ -5,8 +5,8 @@ function setup() {
 function draw() {
 background(255);
  textSize(40);
- floor(text("Rx: " + rotationX, 100, 100));
- floor(text("Ry: " + rotationY, 100, 150));
- floor(text("Rz: " + rotationZ, 100, 200));
+ floor(text("Rx: " + rotationX), 100, 100);
+ floor(text("Ry: " + rotationY), 100, 150);
+ floor(text("Rz: " + rotationZ), 100, 200);
 }
   
