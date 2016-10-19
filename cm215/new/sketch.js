1e5490e1be7bb6769893100f041b6555ea393458
samirachour <achoursamir82@gmail.com>
2016-10-19 11:04:35 -0500

    hh

--- cm215/new/sketch.js
+++ cm215/new/sketch.js
@@ -1,12 +1,22 @@
+var ballSize;
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  ellipseMode(CENTER);
+  var ballSize = 200;
 }
 
 function draw() {
-background(255);
+  
+ drawBall();
+ 
+ background(255);
  textSize(40);
  (text("Rx: " + floor(rotationX), 100, 100));
  (text("Ry: " + floor(rotationY), 100, 150));
  (text("Rz: " + floor(rotationZ), 100, 200));
 }
   
+function drawBall(){
+ fill(200);
+ ellipse(windowWidth/2,windowHeight/2, ballSize, ballSize) 
+}
