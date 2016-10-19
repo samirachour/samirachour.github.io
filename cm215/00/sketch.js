235722bd501298ae7d855dc36dab5c3c04fd32c8
samirachour <achoursamir82@gmail.com>
2016-10-19 08:59:29 -0500

    sdvsdvsd

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -13,40 +13,17 @@ function setup() {
 function draw() {
   background(255);
   
-  for(var i=0;i<touches.length;i++){
-    
-    
+  ellipse(touches[i].x,touches[i].y,50,50);   
     if((i+1)<touches.length){
      
-      beginShape();
-   // fill(255,0,0);
-vertex(touches[i].x, touches[i].y);
-
-vertex(touches[i].x, touches[i].y);
-
-vertex(touches[i+1].x, touches[i+1].y);
-
-vertex(touches[i+1].x, touches[i+1].y);
-endShape(CLOSE);
-    //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
-    }
- else{
- 
+fill(150,150);
 beginShape();
-//fill(255,0,0);
+for(var i=0;i<touches.length;i++){
 vertex(touches[i].x, touches[i].y);
 
-vertex(touches[i].x, touches[i].y);
-
-vertex(touches[0].x, touches[0].y);
-
-vertex(touches[0].x, touches[0].y);
 endShape(CLOSE);
-   // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
-}
-                                   
     
-   ellipse(touches[i].x,touches[i].y,75); 
+    }
   }
 }
 
