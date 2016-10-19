27e0ba9756b3bcd1bf121e520c2713d0a58d4ef4
samirachour <achoursamir82@gmail.com>
2016-10-19 11:07:31 -0500

    shsfh

--- cm215/new/sketch.js
+++ cm215/new/sketch.js
@@ -2,21 +2,21 @@ var ballSize;
 function setup() {
   createCanvas(windowWidth,windowHeight);
   ellipseMode(CENTER);
-  var ballSize = 200;
+  ballSize = 50;
 }
 
 function draw() {
   
- drawBall();
- 
  background(255);
+ drawBall();
  textSize(40);
+ 
  (text("Rx: " + floor(rotationX), 100, 100));
  (text("Ry: " + floor(rotationY), 100, 150));
  (text("Rz: " + floor(rotationZ), 100, 200));
 }
   
 function drawBall(){
- fill(200);
+ fill(120);
  ellipse(windowWidth/2,windowHeight/2, ballSize, ballSize) 
 }
