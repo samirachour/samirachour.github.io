76f48ff228eac837becdf9c46f2ade45962935a1
samirachour <achoursamir82@gmail.com>
2016-10-18 21:28:58 -0500

    khkg

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -14,7 +14,7 @@ function draw() {
   background(255);
   
   for(var i=0;i<touches.length;i++){
-    ellipse(touches[i].x,touches[i].y);         
+             
     if((i+1)<touches.length){
      
       beginShape();
@@ -45,7 +45,7 @@ endShape(CLOSE);
 }
                                    
     
-   
+   ellipse(touches[i].x,touches[i].y);
   }
 }
 
