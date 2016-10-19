bf9894db7139015e20334f1dfdf76ec4e2f06f4e
samirachour <achoursamir82@gmail.com>
2016-10-18 21:16:10 -0500

    dgyt

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -18,13 +18,13 @@ function draw() {
     if((i+1)<touches.length){
      
       beginShape();
-   stroke(random(255),random(255),random(255));  
+   fill(random(255),random(255),random(255));  
 vertex(touches[i].x, touches[i].y);
-stroke(random(255),random(255),random(255));
+fill(random(255),random(255),random(255));
 vertex(touches[i].x, touches[i].y);
-stroke(random(255),random(255),random(255));
+fill(random(255),random(255),random(255));
 vertex(touches[i+1].x, touches[i+1].y);
-stroke(random(255),random(255),random(255));
+fill(random(255),random(255),random(255));
 vertex(touches[i+1].x, touches[i+1].y);
 endShape(CLOSE);
     //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
@@ -32,13 +32,13 @@ endShape(CLOSE);
  else{
  
 beginShape();
-stroke(random(255),random(255),random(255));
+//fill(random(255),random(255),random(255));
 vertex(touches[i].x, touches[i].y);
-stroke(random(255),random(255),random(255));
+//fill(random(255),random(255),random(255));
 vertex(touches[i].x, touches[i].y);
-stroke(random(255),random(255),random(255));
+//fill(random(255),random(255),random(255));
 vertex(touches[0].x, touches[0].y);
-stroke(random(255),random(255),random(255));
+//fill(random(255),random(255),random(255));
 vertex(touches[0].x, touches[0].y);
 endShape(CLOSE);
    // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
