d34095ad70ad413eff9bdd5365c83273297cd1e1
samirachour <achoursamir82@gmail.com>
2016-10-19 09:18:57 -0500

    oser

--- cm215/00/sketch.js
+++ cm215/00/sketch.js
@@ -12,7 +12,8 @@ function setup() {
 
 function draw() {
   background(255);
-   for(var i=0;i<touches.length;i++){
+  noFill(0);
+  for(var i=0;i<touches.length;i++){
   ellipse(touches[i].x,touches[i].y,50,50);   
   if((i+1)<touches.length){
   }   
