ce634661e08f25ad32c1edaf9bc539e48448ab13
samirachour <achoursamir82@gmail.com>
2016-10-19 10:08:12 -0500

    hah

---
+++ cm215/new/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+background(255);
+ textSize(40);
+ text("Rx: " + rotationX, 100, 100);
+ text("Ry: " + rotationY, 100, 150);
+ text("Rz: " + rotationZ, 100, 200);
+}
+  
