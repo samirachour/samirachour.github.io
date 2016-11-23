239b7b53ecc9966f51c9d49943b3bd97ed60b555
samirachour <achoursamir82@gmail.com>
2016-11-22 20:33:23 -0600

    poiuytre

--- cm215/02/sketch.js
+++ cm215/02/sketch.js
@@ -41,7 +41,7 @@ function draw() {
 
   background(255);
   drawBall();
-  textSize(40);
+  textSize(20);
   fill(0, 0, 255),
   /*text("Rx: " + floor(rotationX), 100, 100);
   text("Ry: " + floor(rotationY), 100, 150);
